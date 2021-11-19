c797b9b52e8f82457c71059f9394540e6d700830
osadavc <osadavidath@gmail.com>
2021-11-19 13:20:02 +0530

    add grid columns to plain css

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,7 @@ module.exports = {
   purge: ["./src/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {
-      gridTemplateColumns: {
-        "product-grid": "repeat(auto-fill, minmax(360px, 1fr));",
-        "product-grid-mobile":
-          "grid-template-columns: repeat(auto-fill, minmax(100%, 1fr));",
-      },
-    },
+    extend: {},
   },
   variants: {
     extend: {},
